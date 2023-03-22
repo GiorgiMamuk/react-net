257f91dd6960f68b9179717d90e956430faaaea6
GiorgiMamuk <gm.mamukashvili@gmail.com>
2023-03-22 17:56:40 +0400

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar=()=>{
+    return (
+        <nav className="navbar">
+            <h1>The Dojo Blog</h1>
+            <div className="links">
+                <a href="https://rs.ge/">Home</a>
+                <a href="https://www.youtube.com/" >New Blog</a>
+            </div>
+        </nav>
+    )
+}
+export default Navbar;
